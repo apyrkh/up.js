01e4aa0be1753989a7b8046881ed7e9f64e110e3
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2020-01-01 15:21:32 +0300

    Actualized sources

--- src/up.web.js
+++ src/up.web.js
@@ -1,54 +1,25 @@
-/**
- * up.js - async loading resources on demand
- *
- * Version: 1.0
- * Author: Aliaksandr Pyrkh
- * Contact: alexpyrkh@gmail.com
- *
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Aliaksandr Pyrkh
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 (function(window) {
-  'use strict';
-  var document = window.document;
-  var createElement = 'createElement';
-  var appendChild = 'appendChild';
+  const document = window.document;
+  const createElement = 'createElement';
+  const appendChild = 'appendChild';
   window['up'] = {
     // append a JS element to the document
-    js: function(src) {
-      var script = document[createElement]('script');
+    js: function(src, onLoad, async) {
+      const script = document[createElement]('script');
       script.src = src;
-      script.async = false;
+      script.onload = onLoad;
+      script.async = async;
       document.head[appendChild](script);
-      return this
+      return this;
     },
 
     // append a link element to the document
     link: function(rel, href) {
-      var link = document[createElement]('link');
+      const link = document[createElement]('link');
       link.rel = rel;
       link.href = href;
       document.head[appendChild](link);
-      return this
+      return this;
     }
   };
-})(function() {return this}());
+})(window);
